90878e8dfb89086b69046154f5add87bb7fc5b08
mgerb42 <mgerb42@gmail.com>
2015-04-01 18:41:24 -0500

    Initial Commit
    Initial Commit

---
+++ site/index.php
@@ -0,0 +1,196 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>City of Grand Meadow</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+	<link rel="stylesheet" type="text/css" href="index.css">
+	<link href='http://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'>
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="../bootstrap/assets/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- Custom styles for this template -->
+    <link href="carousel.css" rel="stylesheet">
+  </head>
+
+  <body>
+  
+  <div id="fb-root"></div>
+
+
+
+	<div class="header">
+	
+      <!-- Static navbar -->
+      <nav class="navbar navbar-default navbar-static-top">
+        <div class="container">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">GM</a>
+          </div>
+          <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+              <li class="active"><a href="#">Home</a></li>
+			  
+              <li class="dropdown">
+				<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">City Bill Pay <span class="caret"></span></a>
+                <ul class="dropdown-menu" role="menu">
+                  <li><a href="https://www.govpaynow.com/gps/user/plc/4942" target="_blank">Utilities</a></li>
+				  <li><a href="https://www.govpaynow.com/gps/user/plc/4941" target="_blank">City Payments</a></li>
+				  <li><a href="https://www.govpaynow.com/gps/user/plc/4940" target="_blank">Police Department</a></li>
+                </ul>
+			  </li>
+			  
+              <li class="dropdown">
+				<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Information <span class="caret"></span></a>
+                <ul class="dropdown-menu" role="menu">
+                  <li><a href="http://gm.k12.mn.us/" target="_blank"">Grand Meadow School</a></li>
+				  <li><a href="#" target="_blank">Library</a></li>
+				  <li class="divider"></li>
+				  <li class="dropdown-header">Parks and Rec</li>
+                  <li><a href="#">City Park</a></li>
+                  <li><a href="#">Veteran's Memorial Park</a></li>
+				  <li><a href="#">Pine Lawn</a></li>
+				  <li class="divider"></li>
+				  <li><a href="#">Directory</a></li>
+				  <li><a href="#">Other Local Services</a></li>
+                </ul>
+			  </li>
+            
+			<li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Departments <span class="caret"></span></a>
+                <ul class="dropdown-menu" role="menu">
+                  <li><a href="#">Fire Department</a></li>
+                  <li><a href="#">Ambulence</a></li>
+                  <li><a href="#">Police Department</a></li>
+				  <li><a href="#">EDA</a></li>
+                </ul>
+              </li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="#">Contact</a></li>
+              <li><a href="#">Login</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div><!--/.container-fluid -->
+      </nav>
+
+
+	 
+	  
+	  
+	  <div class="container">
+				<h1 class="header-text text-center color-white">City of Grand Meadow</h1>
+	  </div>
+	  </div>
+	  
+	  <hr>
+	  
+	  
+	  
+	  <div class="container">
+	  
+		<div class="jumbotron full-transparent">
+		
+		
+				
+				<h1 class="text-center"><span>Welcome to Grand Meadow!</span></h1>
+					
+				<p>
+					We are excited about using this site to communicate with our residents, businesses and visitors. Grand Meadow is located on Highway 16, between Rochester and Austin, in Mower County.
+				</p>
+				<br>
+				<p>
+					A bright sunny morning in the 1850's, as the story goes, a gentleman's gaze encompassed the beautiful and peaceful prairie grass landscape and proclaimed, "What a Grand Meadow"! Thus, our town's name was conceived. Grand Meadow was established in 1862, four years after the statehood of Minnesota. Even though the prairie grass landscape is now covered with brick and mortar, the "Grand Meadow" still offers a beautiful and peaceful existence to our residents.
+				</p>
+				
+				<p>
+					Grand Meadow is now a modern and progressive community. We offer a "State of the Art" school, Mayo Health Satellite, Four churches, Assisted Living/Nursing Home facility, active ambulance and fire department, three parks, city library, new housing as well as establiched homes for sale. We pride ourselves in providing a safe and tranquil life for families who want to leave the big city behind. We are proud of our community and welcome you to be part of our "little piece of heaven" in our "Grand Meadow".
+				</p>
+			
+		</div>
+	  
+	  
+	  
+	  
+	  
+	 <hr class="hr-normal">
+	  
+	  
+	  
+		
+		
+		
+		<div class="row">
+			<h1 class="text-center">City of Grand Meadow Residents</h1>
+			<div class="col-lg-6 ">
+				<p>The City of Grand Meadow is asking it’s water customers to monitor the temperature of their water and to continue to monitor the temperature through March. 
+				The reason for this is that the frost level in the ground is at a depth not seen in the last 20 to 30 years. To test, run your cold water line from a faucet for 
+				5 minutes then check the temperature of the water. If the water temperature gets below 40 degrees Fahrenheit, you should start running a pencil-width stream of water 
+				from the faucet. After you start running the water please contact City Hall so that the city can monitor the issue city wide.
+				</p>
+				
+			</div>
+			
+			<div class="col-lg-6 ">
+				<p>The Grand Meadow City Council and the water/sewer department would like to remind you DO NOT put rags and or clothing in the sewer system. 
+				This has been an ongoing issue in the city and as of the last 2 months has been extremely costly to the city. The pump in the Pheasant Run area 
+				has needed work done 3 times in the last 2 months, and if this persists the cost could be accessed to the residents. This reminder is for all Grand Meadow residents regardless of location.
+				</p>
+				
+			</div>
+			
+			
+			
+			<div class="col-md-12">
+			<h4 class="text-center">Thank-you, City of Grand Meadow</h4>
+		
+			</div>
+		
+		</div>
+		
+		<hr class="hr-normal">
+		
+    </div><!-- /.container -->
+
+	 <!-- FOOTER -->
+    <div class="navbar navbar-default navbar-static-bottom">
+			<p class="text-center">Grand Meadow City Hall - 112 Grand Avenue East - <br>
+									PO Box 38, Grand Meadow, MN 55936 - Telephone # 507-754-5280 - Email - cityofgm@hmtel.com</p>
+		
+	</div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script src="../bootstrap/js/bootstrap.min.js"></script>
+    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+    <script src="../bootstrap/js/vendor/holder.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../bootstrap/assets//ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
